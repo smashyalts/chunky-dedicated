d2f25522f29a88fdc1f524937a0318a45d3859d8
smashyalts <smashyalts@gmail.com>
2024-02-27 09:48:00 +0100

    hopefully this works

--- chunky-dedicated/src/main/java/com/funniray/chunkydedicated/ChunkyDedicated.java
+++ chunky-dedicated/src/main/java/com/funniray/chunkydedicated/ChunkyDedicated.java
@@ -10,18 +10,23 @@ import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.popcraft.chunky.api.ChunkyAPI;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.*;
 
 import java.io.*;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 import java.util.zip.ZipOutputStream;
 
 public final class ChunkyDedicated extends JavaPlugin {
-    String sourceFile = "world";
-    FileOutputStream fos = new FileOutputStream("compressed.zip");
-    ZipOutputStream zipOut = new ZipOutputStream(fos);
     ChunkyAPI chunky;
+    Random rand = new Random();
+    int key = rand.nextInt(10000000);
 
     public ChunkyDedicated() throws FileNotFoundException {
     }
@@ -51,48 +56,9 @@ public final class ChunkyDedicated extends JavaPlugin {
                 getLogger().info("Tasks are remaining, server won't close");
                 return;
             }
-            getLogger().info("All tasks finished, closing server...");
-            File fileToZip = new File(sourceFile);
-            FileInputStream fis = null;
+            getLogger().info("All tasks finished, uploading files to r2 and closing server...");
             try {
-                fis = new FileInputStream(fileToZip);
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
-            }
-            ZipEntry zipEntry = new ZipEntry(fileToZip.getName());
-            try {
-                zipOut.putNextEntry(zipEntry);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-
-            byte[] bytes = new byte[1024];
-            int length;
-            while(true) {
-                try {
-                    if (!((length = fis.read(bytes)) >= 0)) break;
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-                try {
-                    zipOut.write(bytes, 0, length);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-
-            try {
-                zipOut.close();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            try {
-                fis.close();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            try {
-                fos.close();
+                ZipDirectory.main("world");
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
@@ -100,10 +66,62 @@ public final class ChunkyDedicated extends JavaPlugin {
                     .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("https://" + getConfig().get("cloudflare-account-id") + ".r2.cloudflarestorage.com", "auto"))
                     .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(getConfig().get("cloudflare-access-key").toString(), getConfig().get("cloudflare-secret-key").toString())))
                     .build();
-
-            s3.putObject(getConfig().get("bucket-name").toString(), "compressed.zip", new File("compressed.zip"));
+            multipartUploadWithS3Client("world.zip");
             getServer().shutdown();
         });
+    }
+
+    public void multipartUploadWithS3Client(String filePath) {
+
+        // Initiate the multipart upload.
+        CreateMultipartUploadResponse createMultipartUploadResponse = S3Client.create().createMultipartUpload(b -> b
+                .bucket(getConfig().getString("bucket-name"))
+                .key(String.valueOf(key)));
+        String uploadId = createMultipartUploadResponse.uploadId();
+
+        // Upload the parts of the file.
+        int partNumber = 1;
+        List<CompletedPart> completedParts = new ArrayList<>();
+        ByteBuffer bb = ByteBuffer.allocate(1024 * 1024 * 5); // 5 MB byte buffer
+
+        try (RandomAccessFile file = new RandomAccessFile(filePath, "r")) {
+            long fileSize = file.length();
+            int position = 0;
+            while (position < fileSize) {
+                file.seek(position);
+                int read = file.getChannel().read(bb);
+
+                bb.flip(); // Swap position and limit before reading from the buffer.
+                UploadPartRequest uploadPartRequest = UploadPartRequest.builder()
+                        .bucket(getConfig().getString("bucket-name"))
+                        .key(String.valueOf(key))
+                        .uploadId(uploadId)
+                        .partNumber(partNumber)
+                        .build();
+
+                UploadPartResponse partResponse = S3Client.create().uploadPart(
+                        uploadPartRequest,
+                        RequestBody.fromByteBuffer(bb));
+
+                CompletedPart part = CompletedPart.builder()
+                        .partNumber(partNumber)
+                        .eTag(partResponse.eTag())
+                        .build();
+                completedParts.add(part);
+
+                bb.clear();
+                position += read;
+                partNumber++;
+            }
+        } catch (IOException ignored) {
+        }
+
+        // Complete the multipart upload.
+        S3Client.create().completeMultipartUpload(b -> b
+                .bucket(getConfig().getString("bucket-name"))
+                .key(String.valueOf(key))
+                .uploadId(uploadId)
+                .multipartUpload(CompletedMultipartUpload.builder().parts(completedParts).build()));
     }
 
     private boolean isFinished() {
