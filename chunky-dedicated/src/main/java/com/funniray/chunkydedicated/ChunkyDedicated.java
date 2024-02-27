f517d12c398ab41bfb863ba4ad346445ded9892b
smashyalts <smashyalts@gmail.com>
2024-02-27 10:42:54 +0100

    hopefully this works now

--- chunky-dedicated/src/main/java/com/funniray/chunkydedicated/ChunkyDedicated.java
+++ chunky-dedicated/src/main/java/com/funniray/chunkydedicated/ChunkyDedicated.java
@@ -1,36 +1,34 @@
 package com.funniray.chunkydedicated;
+
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
-import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.*;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.popcraft.chunky.api.ChunkyAPI;
-import software.amazon.awssdk.core.sync.RequestBody;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.*;
 
-import java.io.*;
-import java.nio.ByteBuffer;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Random;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
-import java.util.zip.ZipOutputStream;
 
 public final class ChunkyDedicated extends JavaPlugin {
     ChunkyAPI chunky;
+    String bucketName = getConfig().getString("bucket-name");
+
     Random rand = new Random();
     int key = rand.nextInt(10000000);
 
-    public ChunkyDedicated() throws FileNotFoundException {
-    }
-
+    AmazonS3 s3 = AmazonS3ClientBuilder.standard()
+            .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("https://" + getConfig().get("cloudflare-account-id") + ".r2.cloudflarestorage.com", "auto"))
+            .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(getConfig().get("cloudflare-access-key").toString(), getConfig().get("cloudflare-secret-key").toString())))
+            .build();
     @Override
     public void onEnable() {
         saveDefaultConfig();
@@ -57,15 +55,12 @@ public final class ChunkyDedicated extends JavaPlugin {
                 return;
             }
             getLogger().info("All tasks finished, uploading files to r2 and closing server...");
+            Objects.requireNonNull(Bukkit.getWorld("world")).save();
             try {
                 ZipDirectory.main("world");
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-            AmazonS3 s3 = AmazonS3ClientBuilder.standard()
-                    .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("https://" + getConfig().get("cloudflare-account-id") + ".r2.cloudflarestorage.com", "auto"))
-                    .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(getConfig().get("cloudflare-access-key").toString(), getConfig().get("cloudflare-secret-key").toString())))
-                    .build();
             multipartUploadWithS3Client("world.zip");
             getServer().shutdown();
         });
@@ -73,56 +68,48 @@ public final class ChunkyDedicated extends JavaPlugin {
 
     public void multipartUploadWithS3Client(String filePath) {
 
-        // Initiate the multipart upload.
-        CreateMultipartUploadResponse createMultipartUploadResponse = S3Client.create().createMultipartUpload(b -> b
-                .bucket(getConfig().getString("bucket-name"))
-                .key(String.valueOf(key)));
-        String uploadId = createMultipartUploadResponse.uploadId();
+        InitiateMultipartUploadRequest initRequest = new InitiateMultipartUploadRequest(bucketName, String.valueOf(key));
+        InitiateMultipartUploadResult initResponse = s3.initiateMultipartUpload(initRequest);
 
-        // Upload the parts of the file.
-        int partNumber = 1;
-        List<CompletedPart> completedParts = new ArrayList<>();
-        ByteBuffer bb = ByteBuffer.allocate(1024 * 1024 * 5); // 5 MB byte buffer
+        File file = new File(filePath);
+        long contentLength = file.length();
+        long partSize = 5 * 1024 * 1024;
 
-        try (RandomAccessFile file = new RandomAccessFile(filePath, "r")) {
-            long fileSize = file.length();
-            int position = 0;
-            while (position < fileSize) {
-                file.seek(position);
-                int read = file.getChannel().read(bb);
+        try {
 
-                bb.flip(); // Swap position and limit before reading from the buffer.
-                UploadPartRequest uploadPartRequest = UploadPartRequest.builder()
-                        .bucket(getConfig().getString("bucket-name"))
-                        .key(String.valueOf(key))
-                        .uploadId(uploadId)
-                        .partNumber(partNumber)
-                        .build();
+            long filePosition = 0;
+            List<PartETag> partETags = new ArrayList<PartETag>();
+            for (int i = 1; filePosition < contentLength; i++) {
 
-                UploadPartResponse partResponse = S3Client.create().uploadPart(
-                        uploadPartRequest,
-                        RequestBody.fromByteBuffer(bb));
+                partSize = Math.min(partSize, (contentLength - filePosition));
 
-                CompletedPart part = CompletedPart.builder()
-                        .partNumber(partNumber)
-                        .eTag(partResponse.eTag())
-                        .build();
-                completedParts.add(part);
 
-                bb.clear();
-                position += read;
-                partNumber++;
+                UploadPartRequest uploadRequest = new UploadPartRequest()
+                        .withBucketName(bucketName)
+                        .withKey(String.valueOf(key))
+                        .withUploadId(initResponse.getUploadId())
+                        .withPartNumber(i)
+                        .withFileOffset(filePosition)
+                        .withFile(file)
+                        .withPartSize(partSize);
+
+
+                UploadPartResult uploadResult = s3.uploadPart(uploadRequest);
+                partETags.add(uploadResult.getPartETag());
+
+                filePosition += partSize;
             }
-        } catch (IOException ignored) {
-        }
 
-        // Complete the multipart upload.
-        S3Client.create().completeMultipartUpload(b -> b
-                .bucket(getConfig().getString("bucket-name"))
-                .key(String.valueOf(key))
-                .uploadId(uploadId)
-                .multipartUpload(CompletedMultipartUpload.builder().parts(completedParts).build()));
+
+            CompleteMultipartUploadRequest compRequest = new CompleteMultipartUploadRequest(getConfig().getString("bucket-name"), String.valueOf(key),
+                    initResponse.getUploadId(), partETags);
+
+            s3.completeMultipartUpload(compRequest);
+        } catch (Exception e) {
+            s3.abortMultipartUpload(new AbortMultipartUploadRequest(bucketName, String.valueOf(key), initResponse.getUploadId()));
+        }
     }
+
 
     private boolean isFinished() {
         return getServer().getWorlds().stream().map(World::getName).noneMatch(chunky::isRunning);
--- chunky-dedicated/src/main/java/com/funniray/chunkydedicated/ZipDirectory.java
+++ chunky-dedicated/src/main/java/com/funniray/chunkydedicated/ZipDirectory.java
@@ -1,11 +1,5 @@
 package com.funniray.chunkydedicated;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.client.builder.AwsClientBuilder;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
